04d6e92f5b49758ea8ab18aa4d8e1718bc9c9421
Ximena Cantera <A01277310@tec.mx>
2024-02-27 18:24:29 -0600

    Lab 10 - Ya se muestra usando node

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const html_header = `
             <title>Master Shoes web</title>
         </head>
         
-        <body class="max-w-m m-auto Helvica">
+        <body class="max-w-m m-auto">
             <!--TODO  NAVBAR-->
             <div>
                 <nav class="bg-white dark:bg-red-900">
@@ -110,39 +110,14 @@ const html_footer = `
                     <li class="mb-4">
                         <a class="hover:underline">Lunes a viernes de 8:30am a 6:30pm y sábados de 9:30am a 2:00pm.</a>
                     </li>
-                    <li class="mb-4">
-                        <div class="flex botonContacto">
-                            <button class="open-button rounded-lg text-white dark:hover:bg-amber-400 focus:outline-none dark:bg-orange-600 py-1.5 px-14 mb-2 mt-4 " id="open-button"> HECHO POR </button>
-                            <div class="window-background" id="window-background">
-                                <div class="window-container bg-slate-950" id="window-container">
-                                    <button class="close-button" id="close-button">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="icon-x">
-                                            <path 
-                                                d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                    <h2 class="flex items-center justify-center text-4xl font-bold text-white mb-4">Datos de Contacto</h2>
-                                    <div class="flex justify-center">
-                                        <svg class="fill-white mr-1.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10c1.466 0 2.961-.371 4.442-1.104l-.885-1.793C14.353 19.698 13.156 20 12 20c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8v1c0 .692-.313 2-1.5 2-1.396 0-1.494-1.819-1.5-2V8h-2v.025A4.954 4.954 0 0 0 12 7c-2.757 0-5 2.243-5 5s2.243 5 5 5c1.45 0 2.748-.631 3.662-1.621.524.89 1.408 1.621 2.838 1.621 2.273 0 3.5-2.061 3.5-4v-1c0-5.514-4.486-10-10-10zm0 13c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3-1.346 3-3 3z"></path></svg>
-                                        <p class="font-semibold text-white flex justify-center">A01277310@tec.mx</p>
-                                    </div>
-                                    <div class="flex justify-center">
-                                        <svg class="fill-white mr-1.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                            <path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg>
-                                        <p class="font-semibold text-white flex justify-center">771 128 5154</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                   
                 </ul>
             </div>
         </div>
     </footer>
 `;
 
-const productos = [
+/*const productos = [
 {
     nombre: "Plantillas de gel",
     descripcion: "Contamos con plantillas y taloneras de gel de acuerdo a tu nececidad . Con gusto te atenderemos ",
@@ -156,17 +131,18 @@ const productos = [
     precio:20,
     imagen: "https://th.bing.com/th/id/OIP.Yrlj9GsHEpfxjYjyIf4KUQHaHa?rs=1&pid=ImgDetMain",
 
-}];
-
+}];*/
 
 const http = require('http');
+
 const server = http.createServer( (request, response) => {    
     console.log(request.url);
 
-    if(request.url == "/"){
+    if (request.url == "/") {
 
         response.setHeader('Content-Type', 'text/html');
-        response.write(`  
+        response.write(html_header);
+        response.write(`
                 <hr class="mt-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
                 <div>
                     <h4 class="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-black text-center pt-8"  >Nuestros Productos</h4>
@@ -184,7 +160,7 @@ const server = http.createServer( (request, response) => {
                 <div class="columns-3 mx-20">
                     <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-red-900">
                         <a href="#">
-                            <img class="p-8 rounded-t-lg" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/312066103_987718562145825_1707666995852869215_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeEilK4Bk-cQjoLyooTjuNSYjFbDLPpOKUuMVsMs-k4pS_T0blvOazzmqzUUlD7EPylPcDwN4ZcoSIw8ZIfQMaOy&_nc_ohc=URx2ZwM3CBwAX-pC2VP&_nc_ht=scontent-dfw5-1.xx&oh=00_AfBID3cT_TWZCnAJZFMaVsqt0KwzdGlJIhghHp_gfhrrjg&oe=65DCA8DB" alt="product image" />
+                           <!-- <img class="p-8 rounded-t-lg" src="https://estaticos.qdq.com/swdata/photos/450/450414538/185a150ca2344681a20e36ad321dfb11.jpg" alt="product image" /> -->
                         </a>
                         <div class="px-5 pb-5">
                             <a href="#">
@@ -201,7 +177,7 @@ const server = http.createServer( (request, response) => {
                     </div>
                     <div class="mx-4 rounded-lg shadow dark:bg-red-900">
                         <a href="#">
-                            <img class="p-8 rounded-t-lg" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/242537897_1569292223420810_1656489833131288913_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFVgI64jinF5ZchZcxWZhmOfWdE1i8gyRV9Z0TWLyDJFeqMi4BD1uhcua31p7TaJkv5oFywqjHDTU9mMwn4fej5&_nc_ohc=TgeDwWFWwMAAX9AyDbl&_nc_ht=scontent-dfw5-1.xx&oh=00_AfBFf0JLCMH0kLsBwXJLLwaFWwRveVM9kveBeI6brV1dZw&oe=65DC44D5"/>
+                            <!--<img class="p-8 rounded-t-lg" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/242537897_1569292223420810_1656489833131288913_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFVgI64jinF5ZchZcxWZhmOfWdE1i8gyRV9Z0TWLyDJFeqMi4BD1uhcua31p7TaJkv5oFywqjHDTU9mMwn4fej5&_nc_ohc=TgeDwWFWwMAAX9AyDbl&_nc_ht=scontent-dfw5-1.xx&oh=00_AfBFf0JLCMH0kLsBwXJLLwaFWwRveVM9kveBeI6brV1dZw&oe=65DC44D5"/>-->
                         </a>
                         <div class="px-5 pb-5">
                             <a href="#">
@@ -218,7 +194,7 @@ const server = http.createServer( (request, response) => {
                     </div>
                     <div class="rounded-lg shadow dark:bg-red-900">
                         <a href="#">
-                            <img class="p-8 rounded-t-lg" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/242537897_1569292223420810_1656489833131288913_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFVgI64jinF5ZchZcxWZhmOfWdE1i8gyRV9Z0TWLyDJFeqMi4BD1uhcua31p7TaJkv5oFywqjHDTU9mMwn4fej5&_nc_ohc=TgeDwWFWwMAAX9AyDbl&_nc_ht=scontent-dfw5-1.xx&oh=00_AfBFf0JLCMH0kLsBwXJLLwaFWwRveVM9kveBeI6brV1dZw&oe=65DC44D5"/>
+                            <!--<img class="p-8 rounded-t-lg" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/242537897_1569292223420810_1656489833131288913_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFVgI64jinF5ZchZcxWZhmOfWdE1i8gyRV9Z0TWLyDJFeqMi4BD1uhcua31p7TaJkv5oFywqjHDTU9mMwn4fej5&_nc_ohc=TgeDwWFWwMAAX9AyDbl&_nc_ht=scontent-dfw5-1.xx&oh=00_AfBFf0JLCMH0kLsBwXJLLwaFWwRveVM9kveBeI6brV1dZw&oe=65DC44D5"/>-->
                         </a>
                         <div class="px-5 pb-5">
                             <a href="#">
@@ -237,28 +213,6 @@ const server = http.createServer( (request, response) => {
                 <hr class="mt-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
 
             
-                <script>
-                    const windowBackground = document.getElementById('window-background'),
-                    windowContainer = document.getElementById('window-container'),
-                    openButton = document.getElementById('open-button'),
-                    closeButton = document.getElementById('close-button')
-                
-                openButton.addEventListener('click', () => windowBackground.style.display = 'flex')
-        
-                const closeWindow = () => {
-                    windowContainer.classList.add('close')
-        
-                    setTimeout(() => {
-                        windowContainer.classList.remove('close')
-                        windowBackground.style.display = 'none'
-                    }, 1000)
-                }
-        
-                closeButton.addEventListener('click', () => closeWindow())
-        
-                window.addEventListener('click', e => e.target == windowBackground && closeWindow());
-    
-                </script>
             </body>
 
         </html>
