6b6f21ab9b7c35bd64d082fde588179559f66635
Ximena Cantera <A01277310@tec.mx>
2024-03-05 23:41:12 -0600

    Lab 10 - Fin

--- app.js
+++ app.js
@@ -17,18 +17,13 @@ const html_header = `
                 <nav class="bg-white dark:bg-red-900">
                     <div class="flex justify-between mx-auto p-4">
                         <div class="flex items-center space-x-3 rtl:space-x-reverse">
-                            <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/241087326_1559743931042306_7376873698654842302_n.png?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeH1QMmnnlcXnuT1iw3MKxug8ljJnbZuJ2HyWMmdtm4nYcLqVcTz1nzivUpvj-UTK2WsKpKwI0oLdyB9DSGSm1eI&_nc_ohc=7cogh4kEYvcAX9GAS8P&_nc_ht=scontent-dfw5-1.xx&oh=00_AfDLfOHlnIfsttkdscIGYLEe8K9sCWg0g8noMJPbwrSXug&oe=65E273D2" class="h-8"/>
+                            <img class="h-8" alt="iconoMasterShoes" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/241087326_1559743931042306_7376873698654842302_n.png?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeH1QMmnnlcXnuT1iw3MKxug8ljJnbZuJ2HyWMmdtm4nYcLqVcTz1nzivUpvj-UTK2WsKpKwI0oLdyB9DSGSm1eI&_nc_ohc=7cogh4kEYvcAX9GAS8P&_nc_ht=scontent-dfw5-1.xx&oh=00_AfDLfOHlnIfsttkdscIGYLEe8K9sCWg0g8noMJPbwrSXug&oe=65E273D2"/>
                             <a href="/"  class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Master Shoes</a>
                         </div>
                         <div class="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
                             <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
                             <a href="#" class="text-white bg-amber-400 hover:bg-orange-600 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-amber-400 focus:outline-none dark:bg-orange-600">Sign up</a>
-                            <button data-collapse-toggle="mega-menu-icons" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-icons" aria-expanded="false">
-                                <span class="sr-only">Open main menu</span>
-                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                                </svg>
-                            </button>
+                            
                         </div>
                     </div>
                 </nav>
@@ -37,63 +32,64 @@ const html_header = `
 
 const html_footer = `
     <!--TODO INFORMACIÓN EN FOOTER-->
+            <footer class="bg-white dark:bg-red-900">
+                <div class="mx-auto w-full max-w-screen-xl">
+                <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Preguntas</h2>
+                        <ul class="dark:text-white font-medium">
+                            <li class="mb-4">
+                                <a href="#" class=" hover:underline"></a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="/PregLab1" class="hover:underline">Laboratorio 1</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="/PregLab3" class="hover:underline">Laboratorio 3</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="/PregLab4" class="hover:underline">Laboratorio 4</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="/PregLab6" class="hover:underline">Laboratorio 6</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contacto</h2>
+                        <ul class="dark:text-white font-medium">
+                            <li class="mb-4">
+                                <a class="hover:underline">mastershoespr@gmail.com </a>
+                            </li>
+                            <li class="mb-4">
+                                <a  class="hover:underline">Teléfono: 55+ 1234-5678</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="https://www.instagram.com/mastershoespr?igsh=a2VraWVkbzF5Nmdl" class="hover:underline">Instagram</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="https://www.facebook.com/mastershoespr" class="hover:underline">Facebook</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Ubicación y horario</h2>
+                        <ul class="dark:text-white font-medium">
+                            <li class="mb-4">
+                                <a href="#" class="hover:underline">Miguel Hidalgo No. 809 , Local C, Pachuca 42000</a>
+                            </li>
+                            <li class="mb-4">
+                                <a class="hover:underline">Lunes a viernes de 8:30am a 6:30pm y sábados de 9:30am a 2:00pm.</a>
+                            </li>
+                        
+                        </ul>
+                    </div>
+                </div>
+            </footer>
         </body>
     </html>
-    <footer class="bg-white dark:bg-red-900">
-        <div class="mx-auto w-full max-w-screen-xl">
-        <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Preguntas</h2>
-                <ul class="dark:text-white font-medium">
-                    <li class="mb-4">
-                        <a href="#" class=" hover:underline"></a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="/PregLab1" class="hover:underline">Laboratorio 1</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="/PregLab3" class="hover:underline">Laboratorio 3</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="/PregLab4" class="hover:underline">Laboratorio 4</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="/PregLab6" class="hover:underline">Laboratorio 6</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contacto</h2>
-                <ul class="dark:text-white font-medium">
-                    <li class="mb-4">
-                        <a class="hover:underline">mastershoespr@gmail.com </a>
-                    </li>
-                    <li class="mb-4">
-                        <a  class="hover:underline">Teléfono: 55+ 1234-5678</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="https://www.instagram.com/mastershoespr?igsh=a2VraWVkbzF5Nmdl" class="hover:underline">Instagram</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="https://www.facebook.com/mastershoespr" class="hover:underline">Facebook</a>
-                    </li>
-                    
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Ubicación y horario</h2>
-                <ul class="dark:text-white font-medium">
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Miguel Hidalgo No. 809 , Local C, Pachuca 42000</a>
-                    </li>
-                    <li class="mb-4">
-                        <a class="hover:underline">Lunes a viernes de 8:30am a 6:30pm y sábados de 9:30am a 2:00pm.</a>
-                    </li>
-                   
-                </ul>
-            </div>
-        </div>
-    </footer>
+    
 `;
 
 const trabajadores = [
@@ -102,14 +98,12 @@ const trabajadores = [
     Area: "Reparación",
     Correo: "xyimna@gmail.com",
     Local: "av.centro cd.48023",
-    Imagen: "https://th.bing.com/th/id/OIP.0nhF34HRrM7OnP_eGqDv6QAAAA?rs=1&pid=ImgDetMain",
 },
 {
     Nombre: "Yael Cortés",
     Area: "Mantenimiento",
     Correo: "yao@gmail.com",
     Local: "av. boulevard pricnipal cd.48083",
-    Imagen: "https://lavozdelmuro.net/wp-content/uploads/2015/06/personas_con_talento_10-1.jpg",
 }];
 
 const http = require('http');
@@ -219,8 +213,7 @@ const server = http.createServer( (request, response) => {
             html_trabajadores += `                
                 <div class="w-full max-w-sm border border-gray-200 rounded-lg shadow bg-[#E48F45]">
                     <div class="flex flex-col items-center pb-10">
-                        <img class="w-24 h-24 my-6 rounded-full shadow-lg" src="${trabajador.Imagen}" alt="Imagen de ${trabajador.Nombre}">
-                        <h5 class="mb-1 text-xl font-medium dark:text-white">${trabajador.Nombre}</h5>
+                       <h5 class="mb-1 text-xl font-medium dark:text-white">${trabajador.Nombre}</h5>
                         <span class="text-sm dark:text-black">${trabajador.Area}</span>
                         <span class="text-sm dark:text-black">${trabajador.Correo}</span>
                         <span class="text-sm dark:text-black">${trabajador.Local}</span>
@@ -260,10 +253,6 @@ const server = http.createServer( (request, response) => {
                 <label for="Local" class="block mb-2 text-sm font-medium dark:text-black">Local</label>
                 <input type="text" id="Local" name="Local" class="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-slate-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:shadow-sm-light" required />
             </div>
-            <div class="mb-5">
-                <label for="Imagen" class="block mb-2 text-sm font-medium dark:text-black">Imagen</label>
-                <input type="text" id="Imagen" name="Imagen" class="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-slate-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:shadow-sm-light" required />
-            </div>
             <button type="submit" class="text-white dark:hover:bg-amber-400 focus:outline-none dark:bg-orange-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Confirmar</button> 
             </form>
         </div>
@@ -282,20 +271,17 @@ const server = http.createServer( (request, response) => {
             datos.push(dato);
         });
 
-
         return request.on('end', () => {
             const todosDatos = Buffer.concat(datos).toString();
             const Nombre = todosDatos.split('&')[0].split('=')[1];
             const Area = todosDatos.split('&')[1].split('=')[1];
             const Correo = todosDatos.split('&')[2].split('=')[1];
             const Local = todosDatos.split('&')[3].split('=')[1];
-            const Imagen = todosDatos.split('&')[4].split('=')[1];
             trabajadores.push({
                 Nombre: Nombre,
                 Area: Area,
                 Correo: Correo,
                 Local: Local,
-                Imagen: Imagen,
             });
             response.writeHead(302, { 'Location': '/' });
             return response.end();
@@ -304,6 +290,7 @@ const server = http.createServer( (request, response) => {
     } else if(request.url == "/PregLab1" && request.method == "GET"){
         response.setHeader('Content-Type', 'text/html');
         response.write(html_header);
+
         response.write(`
         <div class="mx-60 my-20">
             <h3>PREGUNTAS LAB 1</h3>
@@ -402,6 +389,7 @@ const server = http.createServer( (request, response) => {
     } else if(request.url == "/PregLab3" && request.method == "GET"){
         response.setHeader('Content-Type', 'text/html');
         response.write(html_header);
+
         response.write(`
         <div class="mx-60 my-20">
             <h2>PREGUNTAS LAB. 3</h2>
